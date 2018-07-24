6adc82e335e0cc9ca1a0b208fd82650355cd63f5
Stephen Parsons <zup_dudes@yahoo.com>
2018-07-24 17:36:34 -0400

    Updated Vidly App and my first commit.

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Services.Protocols;
 
 namespace Vidly
 {
@@ -12,6 +14,8 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Shrek"},
+                new Movie() {Id = 2, Name = "Wall-e"}
+            };
+
+            var moviesViewModel = new IndexMovieViewModel()
+            {
+                Movies = movies
+            };
+
+            return View( moviesViewModel );
+        }
+
+ 
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek"};
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        
+    }
+}
